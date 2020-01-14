df613167c9438e83beab68bfa40dd4b1fb789ce7
missmaelyss <marnaud@e2r12p16.42.fr>
2020-01-14 18:49:59 +0100

    Come bonjour

---
+++ src/server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const axios = require("axios");
+const port = process.env.PORT || 4001;
+const index = require("./routes/index");
+const app = express();
+app.use(index);
+const server = http.createServer(app);
+const io = socketIo(server); // < Interesting!
+const getApiAndEmit = async socket => {
+    try {
+      socket.emit("FromAPI", "chibre"); // Emitting a new message. It will be consumed by the client
+    } catch (error) {
+      console.error(`Error: ${error.code}`);
+    }
+}
+
+let interval;
+io.on("connection", socket => {
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+server.listen(port, () => console.log(`Listening on port ${port}`));

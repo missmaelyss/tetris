08e2a220681b184d7b5913d37c8ec6c58b08d341
missmaelyss <marnaud@e2r3p2.42.fr>
2020-01-17 20:41:46 +0100

    un peu moche

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom'
-import React from 'react'
+import React, { useState } from 'react'
 import io from "socket.io-client"
 import './App.css'
 import Game from './Game'
@@ -14,35 +14,71 @@ socket.on('other', (others) => {
     <Game grid={other.grid} other="other"/>
     )
   )
-  ReactDOM.render(
-    element,
-    document.getElementById('other')
-  );
+  // ReactDOM.render(
+  //   element,
+  //   document.getElementById('other')
+  // );
 })
 
-socket.on('me', (me) => {
-  console.log(me)
-  ReactDOM.render(
-    <Game grid={me.grid} other="real" />,
-    document.getElementById('real')
-  );
-  ReactDOM.render(
-    <button onClick={() => 
-      socket.emit('changeMyColor', {name: me.name})}>Change My Color</button>,
-    document.getElementById('button')
-  );
-})
+// socket.on('me', (me) => {
+//   console.log(me)
+//   setMyGrid(me.grid)
+//   ReactDOM.render(
+//     <Game grid={me.grid} other="real" />,
+//     document.getElementById('real')
+//   );
+//   ReactDOM.render(
+//     <button onClick={() => 
+//       socket.emit('changeMyColor', {name: me.name})}>Change My Color</button>,
+//     document.getElementById('button')
+//   );
+// })
 
-const App = () => {
+function App() {
+  // Déclaration d'une nouvelle variable d'état, que l'on appellera “count”
+  const [myGrid, setMyGrid] = useState([]);
+  const [otherGrid, setOtherGrid] = useState([]);
+
+  socket.on('me', (me) => {
+    console.log(me)
+    setMyGrid(me.grid)
+  })
+
+  socket.on('other', (others) => {
+    others.splice(others.findIndex((element) => element.name === name), 1)
+    setOtherGrid(others)
+  })
+
+
   return (
     <div id="app">
-      <div id="real"></div>
-      <div id="other"></div>
+      <div id="real">
+        <Game grid={myGrid} other="real" />
+      </div>
+      <div id="other">
+      {otherGrid.map((other) => (
+          <Game 
+            grid={other.grid}
+            other="other"
+          />
+      ))}
+      </div>
       <div id="button"></div>
       <button onClick={() => socket.emit('changeColor')}>Change All Color</button>
     </div>
-    
   );
 }
+
+// const App = () => {
+//   return (
+//     <div id="app">
+//       <div id="real"></div>
+//       <div id="other"></div>
+//       <div id="button"></div>
+//       <button onClick={() => socket.emit('changeColor')}>Change All Color</button>
+//     </div>
+    
+//   );
+// }
 
 export default App

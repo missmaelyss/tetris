dbde2d953460e836cf41bdd57292a0bae543ce9f
cde-laro <cde-laro@student.42.fr>
2020-02-02 13:22:35 +0100

    TouchScreen

--- src/client/components/Game.js
+++ src/client/components/Game.js
@@ -7,11 +7,12 @@ import Col from 'react-bootstrap/Col'
 import Lobby from "./Lobby.js"
 import {useParams} from 'react-router-dom'
 import {Redirect} from 'react-router-dom'
-const endpoint = '51.91.56.214:4001';
+const endpoint = 'localhost:4001'//'51.91.56.214:4001';
 let socket = false;
 
 let keyReady = true;
-
+var xDown = null;
+var yDown = null;
 const Game = () => {
   let {room, username} = useParams();
 
@@ -57,6 +58,45 @@ const Game = () => {
   if (!room || !username)
     return(<Redirect to="/" />)
 
+
+
+  function handleTouchStart(evt) {
+      xDown = evt.touches[0].clientX;
+      yDown = evt.touches[0].clientY;
+
+  };
+  
+  function handleTouchMove(evt) {
+      if ( ! xDown || ! yDown ) {
+          return;
+      }
+  
+      var xUp = evt.touches[0].clientX;
+      var yUp = evt.touches[0].clientY;
+  
+      var xDiff = xDown - xUp;
+      var yDiff = yDown - yUp;
+      if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+          console.log("Left")
+          socket.emit('move', {direction: -1})
+        } else {
+          console.log("Right")
+          socket.emit('move', {direction: 1})
+        }                       
+      } else {
+          if ( yDiff > 0 ) {
+            console.log("Up")
+            socket.emit('rotate')
+          } else { 
+            console.log("down")
+            socket.emit('move', {direction: 0})
+          }
+        }
+      /* reset values */
+      xDown = null;
+      yDown = null;
+  };
   function keyHandler(event){
     if (keyReady === false) { event.preventDefault();return; }
     
@@ -89,6 +129,8 @@ const Game = () => {
   }
 
   window.addEventListener("keydown", keyHandler)
+  window.addEventListener("touchstart", handleTouchStart)
+  window.addEventListener("touchmove", handleTouchMove)
   return (
     <Container id="content" xs={10} sm={6} md={4} >
       <h1 className="display-4">Room #{room}</h1>
@@ -97,7 +139,7 @@ const Game = () => {
       : ('')
       }
       {(permission === 2 ||  permission === 1) && gameStatus.status !== 'waiting' ? (
-        <Col className="mb-5"><Grid grid={myGrid} type="real" score={myScore} piece={myPiece}/></Col>)
+        <Col onTouchStart={handleTouchStart} onTouchMove={handleTouchMove} className="mb-5"><Grid grid={myGrid} type="real" score={myScore} piece={myPiece}/></Col>)
         : ('')
       }
       { gameStatus.status !== 'waiting' ? (

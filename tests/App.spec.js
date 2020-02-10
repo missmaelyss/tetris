94c143593562bd1e398e924d95b550beaf8bd065
cde-laro <cde-laro@student.42.fr>
2020-02-10 15:42:06 +0100

    fix Coverage

--- tests/App.spec.js
+++ tests/App.spec.js
@@ -1,7 +1,6 @@
 import { expect } from 'chai'
 import React from 'react'
 import { shallow } from 'enzyme'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 
 import App from '../src/client/components/App'

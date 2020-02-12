e8e2844513d3a87399c20df5757666eff3c9bce0
missmaelyss <marnaud@e2r2p22.42.fr>
2020-02-12 17:21:15 +0100

    coucou come

--- tests/Game.spec.js
+++ tests/Game.spec.js
@@ -9,9 +9,14 @@ import Game from '../src/client/components/Game'
 describe('<Game />', () => {
   const fakeURL = 'http://localhost:4001/';
   const mockServer = new Server(fakeURL);
-
   mockServer.on('connection', socket => {
   });
+
+  const map = {};
+  window.addEventListener = jest.fn((event, cb) => {
+    map[event] = cb;
+  });
+  const event = { key: "ArrowLeft", preventDefault: () => {} };
 
   it('renders without crashing', () => {
     
@@ -22,28 +27,80 @@ describe('<Game />', () => {
       )
   })
 
-  // it('receives me from server with socket', () => {
-  //   const wrapper = shallow(
-  //     <MemoryRouter initialEntries={['/42/1234']}>
-  //       <Game/>
-  //     </MemoryRouter>
-  //   )
-  //   var me = {grid: [1,1,1,1], score: 42, piece: [2,2,2,2], permission: 2}
-  //   mockServer.emit('me', me);
-  // })
+  it('receives me from server with socket', () => {
+    const wrapper = shallow(
+      <MemoryRouter initialEntries={['/42/1234']}>
+        <Game/>
+      </MemoryRouter>
+    )
+    var me = {grid: [1,1,1,1], score: 42, piece: [2,2,2,2], permission: 2}
+    mockServer.emit('me', me);
+  })
 
-  it('renders without crashing', () => {
+  it('handles touchstart ', () => {
     const wrapper = mount(
       <MemoryRouter initialEntries={['/42/1234']}>
         <Game/>
       </MemoryRouter>
       )
-
-      wrapper.find('#content').at(0).simulate('keypress', {key: 'ArrowDown'})
-    // wrapper.simulate('touchstart', {key: 'ArrowDown'})
-    // wrapper.simulate('touchmove', {key: 'ArrowDown'})
-
-
-    // wrapper.simulate('touchStart',{touches: [{ clientX: 100, clientY: 0 }]});
+      map.touchstart({touches: [{ clientX: 100, clientY: 0 }]});
   })
+
+  it('handles key ArrowUp', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/42/1234']}>
+        <Game/>
+      </MemoryRouter>
+      )
+      jest.spyOn(event, 'preventDefault');
+      map.keydown(event);
+  })
+
+  it('handles key ArrowLeft', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/42/1234']}>
+        <Game/>
+      </MemoryRouter>
+      )
+      // map.keydown();
+  })
+
+  it('handles other key', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/42/1234']}>
+        <Game/>
+      </MemoryRouter>
+      )
+      map.keydown({ key: 'Test' });
+  })
+
+  it('handles touchmove with no xDown and yDown', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/42/1234']}>
+        <Game/>
+      </MemoryRouter>
+      )
+      map.touchmove({touches: [{ clientX: 100, clientY: 0 }]});
+  })
+
+  it('handles touchmove with xDown and yDown', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/42/1234']}>
+        <Game/>
+      </MemoryRouter>
+      )
+      map.touchstart({touches: [{ clientX: 100, clientY: 100 }]});
+      map.touchmove({touches: [{ clientX: -200, clientY: 0 }]});
+  })
+
+  it('handles touchmove with xDown and yDown', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/42/1234']}>
+        <Game/>
+      </MemoryRouter>
+      )
+      map.touchstart({touches: [{ clientX: 100, clientY: 100 }]});
+      map.touchmove({touches: [{ clientX: 100, clientY: 0 }]});
+  })
+  
 })
